fb9d0dd5a902c4d14dc72a44faf5ce914269cb63
cadutjdev <cadutjdev@gmail.com>
2021-07-19 12:42:14 -0300

    evento nos botões de som e cor

--- bateria/script.js
+++ bateria/script.js
@@ -1,11 +1,21 @@
 document.body.addEventListener("keyup", (event)=>{
-    playSound|(event.code.toLocaleLowerCase);
+    playSound(event.code.toLowerCase());
 });
 
 function playSound(sound) {
-    let audioElement = document.querySelector(`#s_${sound};`);
+    let audioElement = document.querySelector(`#s_${sound}`);
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`);
 
-    if(audioElement{
+    if(audioElement){
+        audioElement.currentTime = 0;
         audioElement.play();
+    }
+
+    if(keyElement){
+        keyElement.classList.add(`active`);
+
+        setTimeout(()=>{
+            keyElement.classList.remove(`active`);
+        }, 300);
     }
 }
