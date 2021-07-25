a89bbcf2c74602f50dcb507386ce91f7b7b3c46f
cadutjdev <cadutjdev@gmail.com>
2021-07-24 23:48:20 -0300

    Finalizado o protótipo

--- quiz_teste/script.js
+++ quiz_teste/script.js
@@ -4,6 +4,10 @@ let correctAnswers = 0;
 
 showQuestion();
 
+//Events
+document.querySelector('.scoreArea button').addEventListener('click', resetEvent);
+
+//Functions
 function showQuestion() {
     if(questions[currentQuestion]) {
         let q = questions[currentQuestion];
@@ -43,8 +47,31 @@ function optionClickEvent(e){
 }
 
 function finishQuiz() {
+    let points = Math.floor((correctAnswers / questions.length) * 100);
+
+    if(points < 30){
+        document.querySelector('.scoreText1').innerHTML = 'Tá ruim hein!';
+        document.querySelector('.scorePct').style.color = '#FF0000';
+    } else if (points >= 30 && points < 70){
+        document.querySelector('.scoreText1').innerHTML = 'Muito bom!';
+        document.querySelector('.scorePct').style.color = '#FFFF00'; 
+    } else if(points >= 70){
+        document.querySelector('.scoreText1').innerHTML = 'Parabéns!';
+        document.querySelector('.scorePct').style.color = '#0D630D';
+    }
+
+    
+
+    document.querySelector('.scorePct').innerHTML = `Acertou ${points}%`;
+    document.querySelector('.scoreText2').innerHTML = `Você resondeu ${questions.length} questões e acertou ${correctAnswers}.`;
+
     document.querySelector('.scoreArea').style.display = 'block';
     document.querySelector('.questionArea').style.display = 'none';
     document.querySelector('.progress--bar').style.width = '100%';
 
+}
+function resetEvent(){
+    correctAnswers = 0;
+    currentQuestion = 0;
+    showQuestion();
 }
