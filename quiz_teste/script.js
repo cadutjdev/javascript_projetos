257d2bc5a1e8b0e87df98a4bc7ee388ab324689c
cadutjdev <cadutjdev@gmail.com>
2021-07-24 23:03:18 -0300

    terminando a brra de procentagem

--- quiz_teste/script.js
+++ quiz_teste/script.js
@@ -1,5 +1,6 @@
 //Início dos Dados
 let currentQuestion = 0;
+let correctAnswers = 0;
 
 showQuestion();
 
@@ -7,9 +8,43 @@ function showQuestion() {
     if(questions[currentQuestion]) {
         let q = questions[currentQuestion];
 
+        let pct = Math.floor((currentQuestion / questions.length) * 100);
+        document.querySelector('.progress--bar').style.width = `${pct}%`;
+
         document.querySelector('.scoreArea').style.display = 'none';
         document.querySelector('.questionArea').style.display = 'block';
+
+        document.querySelector('.question').innerHTML = q.question;
+
+        let optionsHtml = '';
+        for(let i in q.options){
+            optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span>${q.options[i]}</div>`;
+        }
+        document.querySelector('.options').innerHTML = optionsHtml;
+
+        document.querySelectorAll('.options .option').forEach(item => {
+            item.addEventListener('click', optionClickEvent);
+        })
     } else {
-        //acabaram as questões
+        finishQuiz();
     }
+}
+
+function optionClickEvent(e){
+    let clickOption = parseInt(e.target.getAttribute('data-op'));
+
+    if(questions[currentQuestion].answer === clickOption){
+        correctAnswers++;
+    }
+
+    currentQuestion++;
+    showQuestion();
+    
+}
+
+function finishQuiz() {
+    document.querySelector('.scoreArea').style.display = 'block';
+    document.querySelector('.questionArea').style.display = 'none';
+    document.querySelector('.progress--bar').style.width = '100%';
+
 }
