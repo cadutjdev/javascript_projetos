1a4b45fc45cba69dc4bba638ba3fe6456f06e682
cadutjdev <cadutjdev@gmail.com>
2021-08-01 23:17:43 -0300

    parando no erro do else

--- clima_local/script.js
+++ clima_local/script.js
@@ -4,6 +4,7 @@ document.querySelector('.busca').addEventListener('submit', async (event)=>{
     let input = document.querySelector('#searchInput').value;
 
     if(input !== '') {
+        clearInfo();
         showWarning('Carregando...');
 
         let url = `https:\\api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=afcff316ce9b4ff2f1eb5cace57da4b0&units=metric&lang=pt_br`;
@@ -11,9 +12,43 @@ document.querySelector('.busca').addEventListener('submit', async (event)=>{
         let results = await fetch(url);
         let json = await results.json();
 
-        console.log(json);
+        if(json.cod === 200){
+            showInfo({
+                name: json.name,
+                country: json.country,
+                temp: json.main.temp,
+                temIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.wind.deg
+            })
+        }else{
+            clearInfo();
+            showWarning('Não encontramos esta localização');
+        }else{
+            clearInfo();
+        }
     }
 });
+
+function showInfo(json) {
+    showWarning('');
+
+
+    document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
+    document.querySelector('.temInfo').innerHTML = `${json.temp} <sup>ºC</sup>`;
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`;
+
+    document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${jason.tempIcon}@2x.png`);
+
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`;
+
+    document.querySelector('.resultado').style.display = 'block';
+}
+
+function clearInfo(){
+    showWarning('');
+    document.querySelector('.resultado').style.display = 'none';
+}
 
 function showWarning(msg) {
     document.querySelector('.aviso').innerHTML = msg;
