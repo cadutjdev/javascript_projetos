0825d01e4bee91e75743b0323516117dfef6118f
cadutjdev <cadutjdev@gmail.com>
2021-08-01 22:15:45 -0300

    consumindo API

--- clima_local/script.js
+++ clima_local/script.js
@@ -1,10 +1,17 @@
-document.querySelector('.busca').addEventListener('submit', (event)=>{
+document.querySelector('.busca').addEventListener('submit', async (event)=>{
     event.preventDefault();
 
     let input = document.querySelector('#searchInput').value;
 
     if(input !== '') {
         showWarning('Carregando...');
+
+        let url = `https:\\api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=afcff316ce9b4ff2f1eb5cace57da4b0&units=metric&lang=pt_br`;
+
+        let results = await fetch(url);
+        let json = await results.json();
+
+        console.log(json);
     }
 });
 
