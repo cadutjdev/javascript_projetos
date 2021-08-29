46374b4b408189f5f6c3b6df4974acf4ef0e9222
cadutjdev <cadutjdev@gmail.com>
2021-08-29 14:40:50 -0300

    Criando as funções

--- jogo_da_velha/script.js
+++ jogo_da_velha/script.js
@@ -1,10 +1,46 @@
-// Dados Iniciais
-let quadro = {
+// Inicial Data
+let square = {
     a1: '', a2: '', a3: '',
     b1: '', b2: '', b3: '',
-    c1: '', c2: '', c3: '',
+    c1: '', c2: '', c3: ''
+};
+let player = '';
+let warning = '';
+let playing = false;
+
+reset();
+
+// Events
+document.querySelector('.reset').addEventListener('click', reset);
+
+// Functions
+function reset() {
+    warning = '';
+
+    let random = Math.floor(Math.random() * 2);
+    player = (random === 0) ? 'x' : 'o';
+
+    for(let i in square){
+        square[i] = '';
+    }
+    playing = true;
+
+    renderSquare();
+    renderInfo();
 }
 
-// Eventos
+function renderSquare(){
+    for(let i in square) {
+        let item = document.querySelector(`div[data-item=${i}]`);
+        if(square[i] !== ''){
+            item.innerHTML = square[i];
+        }else {
+            item.innerHTML = '';
+        }
+    }
 
-// Funções
+}
+
+function renderInfo(){
+
+}
